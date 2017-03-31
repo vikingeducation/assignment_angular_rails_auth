b28f3f29193a1bb9294eb77e977e52dc71c2ac94
tt <ttwang.303@gmail.com>
2017-03-31 12:32:17 -0400

    setup

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,5 @@
+sprint = angular.module("sprint", ['ui.router', 'restangular']);
+
+sprint.controller("testCtrl", ["$scope", function($scope) {
+  $scope.hi = "hi hih ihi haisodfhas;oidfhasd;jlf";
+}])
